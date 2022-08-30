59a1a7fa32603c39cbb473d9b610ee546eeace87
Roberto Toapanta <robertocarlos.toapanta@gmail.com>
2022-08-30 10:21:56 -0500

    Create serial.py

---
+++ serial.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+"""
+__file__ = serial.py
+__author__ = rotoapanta "Roberto Toapanta"
+__copyright__ = "Copyright 2022, BitTech"
+__credits__ = ["Roberto Toapanta, Giovanny Toapanta"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Roberto Toapanta"
+__email__ = "robertocarlos.toapanta@gmail.com"
+__status__ = "Production"
+__date__ = 30/8/22 10:58
+__description__ = "This project is based on an ESP32-S2 general-purpose development board with
+                    micropython that allows the rgb led to blink."
+__information__ : 
+"""
+
+from time import sleep
+
+print("Hola prueba")
+sleep(10)
+print("Roberto")
